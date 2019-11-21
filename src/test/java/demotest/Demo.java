f900501fb5359357fab967aafa5f8c196a42ba57
pravin <pravinpkalubarme@gmail.com>
2019-11-21 17:50:48 +0530

    second commit

--- src/test/java/demotest/Demo.java
+++ src/test/java/demotest/Demo.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Demo {
-
+// this is demo main class
     public static void main(String[] args) throws IOException {
         FileInputStream file= new FileInputStream("C:\\Users\\pravi\\Desktop\\1Single User.xls");
         double value = WorkbookFactory.create(file).getSheet("Sheet1").getRow(1).getCell(0).getNumericCellValue();
